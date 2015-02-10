2cdd3d419b0d61bc1bb1479a32eb4c0954a3dd2b
Gunes Bayir <gunesbayir@Guness-MacBook-Air.local>
2015-02-10 18:05:33 +0200

    error reporting

--- system/core/Common.php
+++ system/core/Common.php
@@ -1,4 +1,4 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); error_reporting(E_STRICT);
 /**
  * CodeIgniter
  *
